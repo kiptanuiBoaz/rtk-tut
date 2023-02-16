fab11b68225822a66fe062e538524ee16a42e228
kiptanuiBoaz <boaserem022@gmail.com>
2023-02-16 19:00:33 +0300

    debuged posts slice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -95,6 +95,7 @@ const postsSlice = createSlice({
 
                 // Add any fetched posts to the array
                 state.posts = state.posts.concat(loadedPosts)
+                
             })
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'failed'
